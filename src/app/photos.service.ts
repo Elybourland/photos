d11d432568ca98903ccb99fad253cb706023b9cb
Elybourland <JEBourland@yahoo.com>
2022-02-19 16:39:12 -0800

    make request with getPhoto() and header authorization through unsplash photo API

--- src/app/photos.service.ts
+++ src/app/photos.service.ts
@@ -7,4 +7,12 @@ import { HttpClient } from '@angular/common/http';
 export class PhotosService {
 
   constructor(private http: HttpClient) {}
+
+  getPhoto() {
+    return this.http.get('https://api.unsplash.com/photos/random', {
+      headers: {
+        Authorization: 'Client-ID nDpNkeDstyTFzcwmn10Ue_qW2ha--uva2SWprE3XhBc'
+      }
+    })
+  }
 }
